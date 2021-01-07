e726437d76d55116d4ce95024bbc85c91bab640e
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-01-07 18:35:30 -0500

    Add files via upload

---
+++ LeetCode Questions (Re-Uploaded).py
@@ -0,0 +1,101 @@
+#Name: Terry Su
+#Date: Dec 28, 2020
+#Purpose: Python questions taken from LeetCode to try during the winter 2020-2021
+
+#Date: Jan 7, 2021
+class Solution:
+    def isValidSudoku(self,board):
+
+        false_count = 0
+
+        #check rows
+        for row in board:
+            
+            for index1, element in enumerate(row):
+                
+                for index2, other in enumerate(row):
+                    if element == other and index1 != index2 and element != '.':
+                        false_count += 1
+
+        #check columns
+        column_list = []
+        
+        for x in range(0,9):
+            
+            for column in range(0,9):
+                column_list.append(board[column][x])
+                
+            for index1, element in enumerate(column_list):
+                
+                for index2, other in enumerate(column_list):
+                    if element == other and index1 != index2 and element != '.':
+                        false_count += 1
+            column_list = [] 
+
+        #check each box
+
+        box_list = []
+            
+        #first column of boxes
+        for box_num in [0,3,6]:
+            
+            for x in range(box_num, box_num + 3):
+            
+                for y in range(0,3):
+                    box_list.append(board[x][y])
+
+            for index1, element in enumerate(box_list):
+                
+                for index2, other in enumerate(box_list):
+                    if element == other and index1 != index2 and element != '.':
+                        false_count += 1
+
+            box_list = []
+
+        #second column of boxes
+        for box_num in [0,3,6]:
+            
+            for x in range(box_num, box_num + 3):
+            
+                for y in range(3,6):
+                    box_list.append(board[x][y])
+            
+
+            for index1, element in enumerate(box_list):
+                
+                for index2, other in enumerate(box_list):
+                    if element == other and index1 != index2 and element != '.':
+                        false_count += 1
+
+            box_list = []
+
+        #third column of boxes
+        for box_num in [0,3,6]:
+            
+            for x in range(box_num, box_num + 3):
+            
+                for y in range(6,9):
+                    box_list.append(board[x][y])
+            
+
+            for index1, element in enumerate(box_list):
+                
+                for index2, other in enumerate(box_list):
+                    if element == other and index1 != index2 and element != '.':
+                        false_count += 1
+
+            box_list = []
+        
+
+        if false_count > 0:
+            return False
+
+        else:
+            return True
+
+                
+
+        
+
+    
+    
