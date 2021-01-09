8a6d13844a8ce6ef05bafb3931d4be23d91bf9cc
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-01-09 12:29:36 -0500

    Update LeetCode Questions (Re-Uploaded).py

--- LeetCode Questions (Re-Uploaded).py
+++ LeetCode Questions (Re-Uploaded).py
@@ -3,6 +3,17 @@
 #Purpose: Python questions taken from LeetCode to try during the winter 2020-2021
 
 #Date: Jan 7, 2021
+#Valid Sudoku
+#Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+#Each row must contain the digits 1-9 without repetition.
+#Each column must contain the digits 1-9 without repetition.
+#Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+#Note:
+
+#A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+#Only the filled cells need to be validated according to the mentioned rules.
+
 class Solution:
     def isValidSudoku(self,board):
 
@@ -92,6 +103,44 @@ class Solution:
 
         else:
             return True
+
+#Date: Jan 9, 2021
+#Two sum
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+#You can return the answer in any order.
+        
+def twoSum(nums,target):
+
+    Lanswer = []
+
+    #a simple iteration over the list to check for target sum
+    for index1, integer1 in enumerate(nums):
+        for index2, integer2, in enumerate(nums):
+
+            if len(Lanswer) == 0 and integer1 + integer2 == target and index1 != index2:
+                Lanswer.append(index1)
+                Lanswer.append(index2)
+
+    return Lanswer
+
+#Date: Jan 9, 2021
+
+#Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+#Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+        
+def removeDuplicates(nums):
+
+    #a simple iteration over the list to check for iterations
+    ans = []
+    for x in nums:
+            
+        if x not in ans:
+            ans.append(x)
+
+    return len(ans), ans
 
                 
 
