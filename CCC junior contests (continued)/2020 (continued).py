178b1dc58817ef1941a10af920d1560d75435aea
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-01-23 16:04:19 -0500

    Update 2020 (continued).py

--- CCC junior contests (continued)/2020 (continued).py
+++ CCC junior contests (continued)/2020 (continued).py
@@ -1,6 +1,6 @@
 #Name: Terry Su
 #Date: July 16, 2020
-#Purpose: CCC 2020 junior questions
+#Purpose: CCC 2020 junior questions (full solutions)
 
 #Q1
 print('Q1')
@@ -102,6 +102,106 @@ if ans == 0:
 
 else:
     print('yes')
+
+#Date: January 22, 2021
+#Q5 (exceeded time limit for last 5 tests but correct algorithm)
+    
+import math
+
+M = int(input())
+N = int(input())
+grid = [[]]
+row = []
+
+#initializing grid
+
+for x in range(0,M):
+    row_input = input()
+    row.append(' ')
+
+    for y in range(0,N - 1):
+        
+        num = row_input.find(' ')
+        
+        row.append(int(row_input[0:num]))
+        row_input = row_input.replace(row_input[0:num+1], '', 1)
+  
+
+    row.append(int(row_input))
+    grid.append(row)
+    row = []
+
+
+#solving the question
+
+ans = 0
+use = 0
+
+x = [1] #current factors or (R,C) being tested
+y = [1]
+
+x_copy = [] #all tested factors to reference for repitition
+y_copy = [] 
+
+current_number = grid[x[0]][y[0]] #current number will always be grid[x[0]][y[0]]
+
+factor2 = 0
+
+
+while len(x) > 0:
+
+    for factor in range(1,(int(math.sqrt(current_number))) + 1):
+
+        if current_number % factor == 0: # if it is a factor
+            factor2 = current_number // factor 
+
+            if factor <= M and factor2 <= N:   # if its in range of the grid
+
+                for used in range(0, len(x_copy)):
+                    if x_copy[used] == factor and y_copy[used] == factor2: #if it has been used
+                        use += 1
+
+                if use == 0:
+                    x.append(factor)
+                    x_copy.append(factor)
+
+                    y.append(factor2)
+                    y_copy.append(factor2)
+
+                use = 0
+
+            #now the opposite way where M and N are reversed
+
+            if factor2 <= M and factor <= N:   # if its in range
+
+                for used in range(0, len(x_copy)):
+                    if x_copy[used] == factor2 and y_copy[used] == factor: #if it has been used
+                        use += 1
+
+                if use == 0:
+                    x.append(factor2)
+                    x_copy.append(factor2)
+
+                    y.append(factor)
+                    y_copy.append(factor)
+
+                use = 0
+
+    x.pop(0) #cut both lists by one to move on to next set of factors
+    y.pop(0)
+
+    if len(x) > 0: #check if its the current number
+        current_number = grid[x[0]][y[0]]
+
+        if x[0] == M and y[0] == N: #if it is add one to ans
+            ans +=1
+            break
+
+if ans > 0:
+    print('yes')
+
+else:
+    print('no')
 
     
     
