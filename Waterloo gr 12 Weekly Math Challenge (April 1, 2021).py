90490aee3f8d32ddbfd166a4e9766294a14063de
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-04-02 11:24:01 -0400

    Update Waterloo gr 12 Weekly Math Challenge (April 1, 2021).py

--- Waterloo gr 12 Weekly Math Challenge (April 1, 2021).py
+++ Waterloo gr 12 Weekly Math Challenge (April 1, 2021).py
@@ -5,10 +5,30 @@ import math
 
 ans = 0
 prime = 0
+table = []
+
+def primeTable(x):
+
+    global table
+    
+    count = 0
+
+    #finding all prime numbers from 3 to x
+    for num in range(2,x): 
+        
+        for factor in range(num + 1, int(math.sqrt(num))):
+            if num % factor == 0:
+                count += 1
+
+        if count == 0:
+            table.append(num)
+    
 
 def primeFactors(n):
-    
+
+    global table
     global prime
+    
     prime = 0
     
     #all the 2s that are divisible into n
@@ -17,8 +37,8 @@ def primeFactors(n):
         n = n / 2
           
     # n must be odd at this point
-    # try all odd factors into n until n's square root
-    for i in range(3,int(math.sqrt(n))+1,2):
+    # try all non-two prime factors into n until n's square root
+    for i in table:
           
         #if i is factorable into n, keep trying it until it cant factor anymore
         while n % i == 0:
@@ -33,14 +53,15 @@ def primeFactors(n):
     return prime
 
 
-for num in range(10000,100000):
+primeTable(100) #the largest prime number we will need is up to sqrt(100000) = around 100
     
+for num in range(10000,100000):
+
     primeFactors(num)
     
     if str(num)[0] == str(num)[3] and str(num)[1] == str(num)[4] and str(num)[2] == '0'  and prime == 5:
         ans += 1
 
 print(ans)
-
 
     
