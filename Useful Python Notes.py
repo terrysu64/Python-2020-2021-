1dca56fc25daf7c41a3892b5034b2fe07ab5f0e5
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-04-03 18:02:08 -0400

    Update Useful Python Notes.py

--- Useful Python Notes.py
+++ Useful Python Notes.py
@@ -41,7 +41,9 @@ print(any(element == 0 for element in x))
 #assuming all iterators have a value at that index
 
 #Params:
+#zip(iterator1, iterator2, iterator3 ...)
 #the iterables to zip
+
 
 #Examples:
 a = ("John", "Charles", "Mike")
@@ -62,6 +64,7 @@ print(tuple(x)) #use the tuple() function to display a readable version of the r
 #get() method takes maximum of two parameters:
 
 #Params:
+#dictionary.get(keyname, value)
 #key - key to be searched in a dictionary
 #value (optional) - Value to be returned if the key is NOT found, would return None if not specified
 
@@ -82,6 +85,7 @@ print(dic.get('sdsdsd','cant find'))
 #You can specify the separator, default separator is any whitespace.
 
 #Params:
+#string.split(separator, maxsplit)
 #separator (optional) - Specifies the separator to use when splitting the string. By default any whitespace is a separator
 #maxsplit (optional) -  Specifies how many splits to do. Default value is -1, which is "all occurrences"
 
@@ -89,4 +93,56 @@ print(dic.get('sdsdsd','cant find'))
 txt = 'i am terry'
 print(txt.split())
 print(txt.split(' ', 1))
+
+
+
+
+#THE .INSERT() FUNCTION
+
+#The .insert() method inserts a certain value into a specific index of an array
+#all indices after that index are shifted down by one
+
+#Params:
+#list.insert(index, value)
+#index - which index your want to insert in
+#value - what is the value you want to insert
+
+#Example
+x = ['a','b','c','d']
+x.insert(2, 'hi')
+print(x)
+
+#THE .JOIN() FUNCTION
+
+#The .join() The method takes all items in an iterable and joins them into one string.
+#A string must be specified as the SEPERATOR.
+
+#Params:
+#string.join(iterable)
+#iterable - Any iterable object where all the returned values are strings
+
+#Example
+x = ''
+a = ['a','b','c']
+print(x.join(a))
+print(' '.join(a))
+
+#THE SORTED() FUNCTION
+
+#The sorted() function returns a sorted list of the specified iterable object.
+#You can specify ascending or descending order. Strings are sorted alphabetically, and numbers are sorted numerically.
+
+#Params:
+#sorted(iterable, key=key, reverse=reverse)
+#iterable - The sequence to sort, list, dictionary, tuple etc.
+#key (Optional) - A Function to execute to decide the order. Default is None
+#reverse(Optional) - A Boolean. False will sort ascending, True will sort descending. Default is False
+
+#sorted(iterable, key=key, reverse=reverse)
+
+#Examples:
+l = ['a','g','b']
+print(sorted(l))
+x = [1,3,5,2,5]
+print(sorted(x))
 
