7ba172b15faeb2882d3f8420336a0fcd0fb90c75
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-05-03 20:30:17 -0400

    Delete perfect number generator.py

--- perfect number generator.py
+++
@@ -1,9 +0,0 @@
-count = 0
-for x in range(1,1000000000):
-    count = 0
-    for y in range(1,x + 1):
-        if x / y == x // y:
-            count += y
-    if count / x == 2:
-        print(x)
-        
