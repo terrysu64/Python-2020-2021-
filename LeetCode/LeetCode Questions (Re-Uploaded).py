4bcea5f5c232dca5467068faba1cc8605bc6c9d8
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-05-30 20:30:47 -0400

    Update LeetCode Questions (Re-Uploaded).py

--- LeetCode/LeetCode Questions (Re-Uploaded).py
+++ LeetCode/LeetCode Questions (Re-Uploaded).py
@@ -661,3 +661,45 @@ def combinationSum(candidates,target):
     [res_final.append(x) for x in res if x not in res_final] #removing duplicates
     
     return res_final
+
+#Date: May 30, 2021 (not done)
+#Write a program to solve a Sudoku puzzle by filling the empty cells.
+#**each cell is filled with a string
+
+#A sudoku solution must satisfy all of the following rules:
+#Each of the digits 1-9 must occur exactly once in each row.
+#Each of the digits 1-9 must occur exactly once in each column.
+#Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+#The '.' character indicates empty cells.
+                    
+def solveSudoku(board):
+
+    box_coor = [[(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)], #box coordinates by row
+                [(0,3),(0,4),(0,5),(1,3),(1,4),(1,5),(2,3),(2,4),(2,5)],
+                [(0,6),(0,7),(0,8),(1,6),(1,7),(1,8),(2,6),(2,7),(2,8)],
+                [(3,0),(3,1),(3,2),(4,0),(4,1),(4,2),(5,0),(5,1),(5,2)],
+                [(3,3),(3,4),(3,5),(4,3),(4,4),(4,5),(5,3),(5,4),(5,5)],
+                [(3,6),(3,7),(3,8),(4,6),(4,7),(4,8),(5,6),(5,7),(5,8)],
+                [(6,0),(6,1),(6,2),(7,0),(7,1),(7,2),(8,0),(8,1),(8,2)],
+                [(6,3),(6,4),(6,5),(7,3),(7,4),(7,5),(8,3),(8,4),(8,5)],
+                [(6,6),(6,7),(6,8),(7,6),(7,7),(7,8),(8,6),(8,7),(8,8)]]
+        
+    def possible(value,r,c): #r = row, c = column
+        
+        #check row
+        if value in board[r]:
+            return False
+
+        #check column
+        if value in [x[c] for x in board]:
+            return False
+
+        #check box
+        for box in box_coor:
+            if (r,c) in box:
+                if value in [board[spot[0]][spot[1]] for spot in box]:
+                    return False
+
+        return True
+
+    #make the recursion part of scanning through every empty cell and branching possibilities
