2daade2fa9969cea2c37867e650f239ade57b727
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-06-17 20:26:21 -0400

    Update LeetCode Questions (Re-Uploaded).py

--- LeetCode/LeetCode Questions (Re-Uploaded).py
+++ LeetCode/LeetCode Questions (Re-Uploaded).py
@@ -923,3 +923,47 @@ def firstMissingPositive(self, nums):
         ans += 1
             
     return ans
+
+#Date: June 17, 2021
+#Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+#Symbol       Value
+
+#I             1
+#V             5
+#X             10
+#L             50
+#C             100
+#D             500
+#M             1000
+#For example, 2 is written as II in Roman numeral, just two one's added together.
+#12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+#Roman numerals are usually written largest to smallest from left to right.
+#However, the numeral for four is not IIII. Instead, the number four is written as IV.
+#Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX.
+#There are six instances where subtraction is used:
+
+#I can be placed before V (5) and X (10) to make 4 and 9. 
+#X can be placed before L (50) and C (100) to make 40 and 90. 
+#C can be placed before D (500) and M (1000) to make 400 and 900.
+#Given a roman numeral, convert it to an integer.
+
+def romanToInt(s):
+    
+    roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+    ans = 0
+
+    #Ideally, we can iterate through the string and sum up corresponding values to the symbols one by one
+    #The only exception is in scenarios where there is a sub-string with the value of 4 * 10^x (x = 0 or xEZ+)
+        #to solve this we can check if value of very next index > value at current index and substract value at current index if true
+    
+    for i in range(0, len(s) - 1):
+            
+        if roman[s[i]] < roman[s[i+1]]:
+            ans -= roman[s[i]]
+                
+        else:
+            ans += roman[s[i]]
+                
+    return ans + roman[s[-1]] #for the last index of string so we dont encounter IndexError
