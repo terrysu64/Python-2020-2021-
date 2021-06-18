76142ea1b5eda83a13340d5c1c8a0c4f17c8d6e7
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-06-17 20:16:35 -0400

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -25,3 +25,35 @@ def bubble_sort(arr):
         count -= 1
 
     return arr
+
+def selection_sort(arr):
+
+    count = 0
+
+    while count < len(arr):
+
+        min_val = arr[count]
+
+        for i in range(count,len(arr)):
+            min_val = min(arr[i],min_val)
+
+        min_index = arr.index(min_val)
+        arr[count], arr[min_index] = arr[min_index],arr[count]
+
+        count += 1
+
+    return arr
+
+def insertion_sort(arr):
+
+    for i in range(1,len(arr)):
+
+        curr = arr[i]
+
+        for j in range(0,i):
+            if curr <= arr[j]:
+                arr.insert(j,curr)
+                arr.pop(i+1)
+                break
+                
+    return arr
