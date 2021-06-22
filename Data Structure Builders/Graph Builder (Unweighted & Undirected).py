4cdc6ad45268e2e33fb71d24dfab3ebe611ffe80
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-06-22 16:31:10 -0400

    Update Graph Builder (Unweighted & Undirected).py

--- Data Structure Builders/Graph Builder (Unweighted & Undirected).py
+++ Data Structure Builders/Graph Builder (Unweighted & Undirected).py
@@ -32,6 +32,63 @@ class Graph:
             print(node, self.adjacent_list[node])
         return
             
+    def bfs_traversal(self,start_node):
+
+        visited = []
+        queue = [start_node] 
+
+        while len(queue) != 0: 
+            
+            for connection in self.adjacent_list[queue[0]]: #adding connections/edges of current node into queue
+                
+                if connection not in visited:
+                    queue.append(connection)
+
+            visited.append(queue[0]) #adding current node into visited and moving onto next node in queue
+            queue.pop(0)
+
+        return print(visited)
+
+    def bfs_traversal2(self,start_node): #also gives layer of BFS
+
+        visited = []
+        queue = [(start_node,1)] #node, depth of BFS
+
+        while len(queue) != 0: 
+            
+            for connection in self.adjacent_list[queue[0][0]]:
+                
+                if connection not in [x[0] for x in visited]:
+                    queue.append((connection,queue[0][1]+1))
+
+            visited.append(queue[0])
+            queue.pop(0)
+
+        return print(visited)
+
+    def dfs_traversal(self,start_node):
+
+        visited = []
+
+        def traverse(curr): #start with the initial node
+
+            visited.append(curr)
+            
+            for connection in self.adjacent_list[curr]: #go down one path as deep as possible from initial, then explore connections starting from deepest children
+                if connection not in visited:
+                    traverse(connection)
+            
+            return visited
+
+        return print(traverse(start_node))
+
+        
+
+
+        
+        
+        
+            
 my_graph = Graph()
 my_graph.add_vertex(1)
 my_graph.add_vertex(2)
@@ -39,8 +96,10 @@ my_graph.add_vertex(3)
 my_graph.add_vertex(4)
 my_graph.add_vertex(5)
 my_graph.add_edge(1,3)
-my_graph.add_edge(1,5)
-my_graph.add_edge(4,3)
-my_graph.add_edge(1,5)
-my_graph.add_edge(2,2)
+my_graph.add_edge(3,5)
+my_graph.add_edge(3,4)
+my_graph.add_edge(1,2)
+my_graph.add_edge(1,4)
+
 my_graph.show_connections()
+my_graph.dfs_traversal(1)
