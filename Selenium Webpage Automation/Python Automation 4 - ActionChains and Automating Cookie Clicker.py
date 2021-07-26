20a2e25418580f85e89194bdd68de10f4aadce97
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-07-26 12:15:07 -0400

    Add files via upload

---
+++ Selenium Webpage Automation/Python Automation 4 - ActionChains and Automating Cookie Clicker.py
@@ -0,0 +1,44 @@
+#Date: July 26, 2021
+#Author: Terry Su
+#Purpose: using Selenium action chains to automate cookie clicker
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get('https://orteil.dashnet.org/cookieclicker/')
+driver.maximize_window()
+
+#implicit wait
+driver.implicitly_wait(4)
+
+cookie = driver.find_element_by_id('bigCookie')
+cookie_count = driver.find_element_by_id('cookies')
+items = [driver.find_element_by_id('productPrice' + str(i)) for i in range(3,-1,-1)] #going in reverse lets us check the higher class upgrades first
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+#click the cookie, then check for available upgrades
+while True:
+    actions.perform()
+    count = int(cookie_count.text.split(' ')[0])
+
+    for item in items:
+
+        text = item.get_attribute('textContent')
+
+        if ',' in text:
+            value = int(text[:text.index(',')]+text[text.index(',')+1:])
+        else:
+            value = int(item.get_attribute('textContent'))
+
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_actions.perform()
+
+
+
