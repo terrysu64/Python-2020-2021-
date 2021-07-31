b46f3b52e5f7fbbd9df71bc62e1a72d2f8be1cb9
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-07-31 12:35:54 -0400

    Update LeetCode Questions (Re-Uploaded).py

--- LeetCode/LeetCode Questions (Re-Uploaded).py
+++ LeetCode/LeetCode Questions (Re-Uploaded).py
@@ -1942,3 +1942,31 @@ def searchMatrix(matrix):
         return True
     return False
 
+#Date: July 31, 2021
+#Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+#We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+#You must solve this problem without using the library's sort function.
+
+def sortColors(nums):
+        
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    #Dutch flag algorithm; one-pass and O(n) time
+    #other feasible solutions: bubble sort O(n^2), insertion/selection sort O(2n)
+        
+    low,mid,high = 0,0,len(nums)-1
+        
+    while mid <= high:
+            
+        if nums[mid] == 0:
+            nums[mid],nums[low] = nums[low],nums[mid]
+            low += 1
+            mid += 1
+         
+        elif nums[mid] == 1:
+            mid += 1
+            
+        else:
+            nums[mid],nums[high] = nums[high],nums[mid]
+            high -= 1
