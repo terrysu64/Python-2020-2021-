02c3357ba9aadf1069ffc4e1d4b4156cb8beabcf
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-08-29 20:57:11 -0400

    new leetcode question 😀

--- LeetCode/LeetCode Questions.py
+++ LeetCode/LeetCode Questions.py
@@ -3090,3 +3090,24 @@ def getRow(rowIndex):
         row = [i+j for i,j in zip([0]+row,row+[0])]
         
     return row
+
+#Date: August 29, 2021
+#It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+# At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
+# Return the maximum number of ice cream bars the boy can buy with coins coins.
+# Note: The boy can buy the ice cream bars in any order.
+
+def maxIceCream(costs,coins):
+        
+    #simple O(n log n) greedy algorithm
+    
+    ans = 0
+    
+    for n in sorted(costs):
+        coins -= n
+        if coins >= 0:
+            ans += 1
+            continue
+        break
+        
+    return ans
