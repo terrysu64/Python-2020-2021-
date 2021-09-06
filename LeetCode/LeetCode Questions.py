6132b8a7abf886e9bfa92c53649d00ad045e84fc
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-09-06 11:54:14 -0400

    new leetcode question

--- LeetCode/LeetCode Questions.py
+++ LeetCode/LeetCode Questions.py
@@ -3435,3 +3435,26 @@ class Solution:
             left += 1
             right -= 1
         return True
+
+#Date: September 6, 2021
+#A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+# You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence, and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released. Both arrays are 0-indexed. The 0th key was pressed at the time 0, and every subsequent key was pressed at the exact time the previous key was released.
+# The tester wants to know the key of the keypress that had the longest duration. The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1], and the 0th keypress had a duration of releaseTimes[0].
+# Note that the same key could have been pressed multiple times during the test, and these multiple presses of the same key may not have had the same duration.
+# Return the key of the keypress that had the longest duration. If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+
+def slowestKey(releaseTimes, keysPressed):
+        
+    #O(n) time; for each release time, we compare the current hold time to the previous and make updates depending on the respective outcomes (either longer hold time or same hold time) 
+    
+    ans, prev, max_hold = '',0,0
+    
+    for i,j in enumerate(releaseTimes):
+        if (hold := j-prev) > max_hold:
+            max_hold = hold
+            ans = keysPressed[i]
+        elif hold == max_hold:
+            ans = max(ans,keysPressed[i])
+        prev = j
+    
+    return ans
