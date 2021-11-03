dce41b49381ad2d5e557b497188c6332cea26af2
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-11-03 09:38:48 -0400

    new leetcode question

--- LeetCode/LeetCode Questions.py
+++ LeetCode/LeetCode Questions.py
@@ -4909,3 +4909,30 @@ class Solution:
         dfs(set(),opr,opc)
         
         return self.ans
+
+#Date: November 3, 2021
+#https://leetcode.com/problems/flatten-binary-tree-to-linked-list/submissions/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.prev = None
+
+    def flatten(self, root):
+        
+        #reverse post-order dfs; construct the LL bottom-up. 
+        #Note: Only connections are altered, nodes themselves are never disconnected
+        
+        if not root:
+            return None
+        self.flatten(root.right)
+        self.flatten(root.left)
+        
+        root.right = self.prev
+        root.left = None
+        self.prev = root 
