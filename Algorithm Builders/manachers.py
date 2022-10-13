330efe32420bc7ed6812f67a12b37a95bb76b7ad
Terry Su <limacechan2169@gmail.com>
2022-10-12 21:06:42 -0400

    manachers algorithm

---
+++ Algorithm Builders/manachers.py
@@ -0,0 +1,14 @@
+#max palindrome extension at each index/center
+
+def manachers(S):
+    A = '@#' + '#'.join(S) + '#$'
+    Z = [0] * len(A)
+    center = right = 0
+    for i in range(1, len(A) - 1):
+        if i < right:
+            Z[i] = min(right - i, Z[2 * center - i])
+        while A[i + Z[i] + 1] == A[i - Z[i] - 1]:
+            Z[i] += 1
+        if i + Z[i] > right:
+            center, right = i, i + Z[i]
+    return Z[2:-2:2]
